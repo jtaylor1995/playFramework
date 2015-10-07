504ae1aeb922bd4f09967cfdf6bcb8a7877bee8a
James Taylor <jamestay@kainos.com>
2015-10-07 15:56:04 +0100

    Add Task screen with no functionality

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,32 @@
 package controllers;
 
-import play.*;
 import play.mvc.*;
+import play.data.*;
+import models.*;
+
 
 import views.html.*;
 
 public class Application extends Controller {
 
     public Result index() {
-        return ok(index.render("Hello world"));
+        return redirect(routes.Application.tasks());
     }
+
+    public Result tasks() {
+        return ok(
+                views.html.index.render(Task.all(), taskForm)
+        );
+    }
+
+    public Result newTask() {
+        return TODO;
+    }
+
+    public Result deleteTask(Long id) {
+        return TODO;
+    }
+
+    static Form<Task> taskForm = Form.form(Task.class);
 
 }
