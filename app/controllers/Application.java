e939cfba737366a5b6584b91c318bca6adba3142
James Taylor <jamestay@kainos.com>
2015-10-07 16:16:32 +0100

    Add database configuration

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -20,7 +20,15 @@ public class Application extends Controller {
     }
 
     public Result newTask() {
-        return TODO;
+        Form<Task> filledForm = taskForm.bindFromRequest();
+        if(filledForm.hasErrors()) {
+            return badRequest(
+                    views.html.index.render(Task.all(), filledForm)
+            );
+        } else {
+            Task.create(filledForm.get());
+            return redirect(routes.Application.tasks());
+        }
     }
 
     public Result deleteTask(Long id) {
