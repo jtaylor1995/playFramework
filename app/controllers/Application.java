b6852a8fde016d3e0082b270e31b725392b6335a
James Taylor <jamestay@canary.local>
2015-10-08 14:20:29 +0100

    Added some login functionality

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -11,17 +11,17 @@ import static play.data.Form.form;
 
 public class Application extends Controller {
 
-    public Result index() {
+    public static Result index() {
         return redirect(routes.Application.tasks());
     }
 
-    public Result tasks() {
+    public static Result tasks() {
         return ok(
                 views.html.index.render(Task.all(), taskForm)
         );
     }
 
-    public Result newTask() {
+    public static Result newTask() {
         Form<Task> filledForm = taskForm.bindFromRequest();
         if(filledForm.hasErrors()) {
             return badRequest(
@@ -33,22 +33,41 @@ public class Application extends Controller {
         }
     }
 
-    public Result deleteTask(Long id) {
+    public static Result deleteTask(Long id) {
         Task.delete(id);
         return redirect(routes.Application.tasks());
     }
 
     static Form<Task> taskForm = form(Task.class);
 
-    public Result login() {
+    public static Result login() {
         return ok(
                 views.html.login.render(form(Login.class))
         );
     }
 
-    public Result authenticate() {
-        Form<Login> loginForm = form(Login.class).bindFromRequest();
-        return ok();
+//    public static Result authenticate() {
+//        Form<Login> loginForm = form(Login.class).bindFromRequest();
+//        if (loginForm.hasErrors()) {
+//            return badRequest(views.html.login.render(loginForm));
+//        } else {
+//            session().clear();
+//            session("email", loginForm.get().email);
+//            return redirect(
+//                    routes.Application.index()
+//            );
+//        }
+//    }
+
+    public static Result authenticate() {
+        return redirect(routes.Application.index());
+    }
+
+    public static Boolean authenticateDetails(String email, String password) {
+        if (email.equals("James@Taylor.com") && password.equals("password")) {
+            return Boolean.TRUE;
+        }
+        return Boolean.FALSE;
     }
 
     public static class Login {
@@ -56,6 +75,14 @@ public class Application extends Controller {
         public String email;
         public String password;
 
+
+        public String validate() {
+            if (Application.authenticateDetails(email, password) == Boolean.FALSE) {
+                return "Invalid user or password";
+            }
+            else Application.authenticate();
+            return null;
+        }
     }
 
 }
